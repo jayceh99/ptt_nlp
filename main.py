47fbb235d5ecbebee4eda6a27059823348951eff
jayce <louis821110@hmail.com>
2024-01-02 16:48:33 +0800

    get context done

--- main.py
+++ main.py
@@ -2,42 +2,97 @@ import requests
 import jieba
 from bs4 import BeautifulSoup
 
-class ptt_requests:
+class c_ptt_requests:
     def __init__(self) -> None:
         self.url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
         self.my_headers = {'cookie' : 'over18=1;'}
         self.all_url = []
+        self.index_url = []
 
-    def find_index_url(self) :
+    def f_find_index_url(self) :
         r = requests.get(self.url , headers = self.my_headers)
         soup = BeautifulSoup(r.text , 'html5lib') 
         url = soup.find(class_='btn-group btn-group-paging')
-        index_url  = url.find_all('a' , href = True)
-        index_url = str(index_url[1]['href'])
-        index_url = index_url.replace('/bbs/Gossiping/index' , '').replace('.html' , '')
-        index_url = int(index_url) + 1
-        self.index_url = index_url
+        index_url_number  = url.find_all('a' , href = True)
+        index_url_number = str(index_url_number[1]['href'])
+        index_url_number = index_url_number.replace('/bbs/Gossiping/index' , '').replace('.html' , '')
+        index_url_number = int(index_url_number) + 1
+        for i in range (0,10):
+            url_tmp = index_url_number - i
+            self.index_url.append('https://www.ptt.cc/bbs/Gossiping/index'+str(url_tmp)+'.html')
 
-    def get_all_url(self) :
-        r = requests.get(self.url , headers = self.my_headers)
-        soup = BeautifulSoup(r.text , 'html5lib') 
-        url = soup.select('.title')
-        for i in url:
-            self.all_url.append(i.find('a' , href = True)['href'])
 
-        
+    def f_get_all_url(self) :
+        for k in self.index_url :
+            try :
+                r = requests.get(k , headers = self.my_headers)
+                soup = BeautifulSoup(r.text , 'html5lib') 
+                url = soup.select('.title')
+                for i in url:
+                    self.all_url.append('https://www.ptt.cc'+i.find('a' , href = True)['href'])
+            except :
+                pass
+        print(self.all_url)
 
-    def get_all_context(self) :
-        pass
 
-class ptt_nlp:
+    def f_get_all_context(self) :
+        count = 0
+        shhh = 0
+        push = 0
+        shhh_words = []
+        push_words = []
+        push_word_count = {}
+        shhh_word_count = {}
+        for i in self.all_url:
+            r = requests.get(i , headers = self.my_headers)
+            soup = BeautifulSoup(r.text, "html5lib")
+            reser = soup.select('.push')
+            
+            count += 1
+            for i in reser:
+
+                try:
+                    if '噓' in str(i.find('span' , class_='f1 hl push-tag').get_text()) :
+                        shhh += 1
+                        txt = str(i.find('span' , class_='f3 push-content').get_text().replace(':','').replace(' ',''))
+                        shhh_words.append(txt)
+
+                except:
+                    if '推' in str(i.find('span' , class_='hl push-tag').get_text()) :
+                        push += 1
+                        txt = str(i.find('span' , class_='f3 push-content').get_text().replace(':','').replace(' ',''))
+                        push_words.append(txt)
+
+            for i in push_words:
+                s1_list = jieba.cut(i, cut_all =True)
+                for n  in s1_list :
+                    if n in push_word_count:
+                        push_word_count[n] +=1
+                    else:
+                        push_word_count[n] = 1
+            for i in shhh_words:
+                s1_list = jieba.cut(i, cut_all =True)
+                for n  in s1_list :
+                    if n in shhh_word_count:
+                        shhh_word_count[n] +=1
+                    else:
+                        shhh_word_count[n] = 1
+        print(sorted(push_word_count.items(), key=lambda x:x[1]))
+        print(sorted(shhh_word_count.items(), key=lambda x:x[1]))
+        print(count)
+#print(sorted(word_count.keys()))
+#print(word_count)
+
+class c_ptt_nlp:
     def __init__(self) -> None:
         pass
-    def ptt_jieba(self):
+    def f_ptt_jieba(self):
         pass
 def main():
-    ptt_requests_q = ptt_requests()
-    ptt_requests_q.test()
+    ptt_requests_q = c_ptt_requests()
+    ptt_requests_q.f_find_index_url()
+    ptt_requests_q.f_get_all_url()
+    ptt_requests_q.f_get_all_context()
 if __name__ == '__main__':
     main()
 
