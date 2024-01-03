ffc8778ff244c03addbb0631343b6ec1bfa3e1c5
jayce <louis821110@gmail.com>
2024-01-03 16:30:41 +0800

    del less words

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ class c_ptt_requests:
             url_tmp = index_url_number - i
             self.index_url.append('https://www.ptt.cc/bbs/Gossiping/index'+str(url_tmp)+'.html')
 
-
     def f_get_all_url(self) :
         for k in self.index_url :
             try :
@@ -33,15 +32,13 @@ class c_ptt_requests:
             except :
                 pass
 
-
     def f_get_all_context(self) :
         count = 0
         shhh = 0
         push = 0
         shhh_words = []
         push_words = []
-        push_word_count = {}
-        shhh_word_count = {}
+        
         for i in self.all_url:
             r = requests.get(i , headers = self.my_headers)
             soup = BeautifulSoup(r.text, "html5lib")
@@ -61,8 +58,19 @@ class c_ptt_requests:
                         push += 1
                         txt = str(i.find('span' , class_='f3 push-content').get_text().replace(':','').replace(' ',''))
                         push_words.append(txt)
-
-        for i in push_words:
+        print(count)
+        return shhh_words , push_words
+        
+class c_ptt_nlp:
+    def __init__(self , push_words , shhh_words) -> None:
+        self.push_words = push_words
+        self.shhh_words = shhh_words
+    def f_ptt_jieba(self):
+        push_word_count = {}
+        shhh_word_count = {}
+        del_push = []
+        del_shhh= []
+        for i in self.push_words:
             s1_list = jieba.cut(i, cut_all =True)
             for n  in s1_list :
                 if len(n) > 1 :
@@ -70,7 +78,7 @@ class c_ptt_requests:
                         push_word_count[n] +=1
                     else:
                         push_word_count[n] = 1
-        for i in shhh_words:
+        for i in self.shhh_words:
             s1_list = jieba.cut(i, cut_all =True)
             for n  in s1_list :
                 if len(n) > 1 :
@@ -78,62 +86,31 @@ class c_ptt_requests:
                         shhh_word_count[n] +=1
                     else:
                         shhh_word_count[n] = 1
-                    
-        print(sorted(push_word_count.items(), key=lambda x:x[1]))
-        print(sorted(shhh_word_count.items(), key=lambda x:x[1]))
-        print(count)
-#print(sorted(word_count.keys()))
-#print(word_count)
 
-class c_ptt_nlp:
-    def __init__(self) -> None:
-        pass
-    def f_ptt_jieba(self):
-        pass
+        for i in push_word_count :
+            if push_word_count[i] < 10 :
+                del_push.append(i)
+        for i in del_push :
+            del push_word_count[i]
+            
+        for i in shhh_word_count :
+            if shhh_word_count[i] < 10 :
+                del_shhh.append(i)
+        for i in del_shhh :
+            del shhh_word_count[i]
+
+        print("push_words:\n")
+        print(sorted(push_word_count.items(), key=lambda x:x[1]))
+        print("shhh_words:\n")
+        print(sorted(shhh_word_count.items(), key=lambda x:x[1]))
+        
 def main():
     ptt_requests_q = c_ptt_requests()
     ptt_requests_q.f_find_index_url()
     ptt_requests_q.f_get_all_url()
-    ptt_requests_q.f_get_all_context()
+    shhh_words , push_words = ptt_requests_q.f_get_all_context()
+    ptt_nlp_q = c_ptt_nlp(shhh_words=shhh_words , push_words=push_words)
+    ptt_nlp_q.f_ptt_jieba()
+
 if __name__ == '__main__':
     main()
-
-
-'''
-my_url = "https://www.ptt.cc/bbs/Gossiping/M.1704110086.A.17B.html"
-# 設定Header與Cookie
-my_headers = {'cookie': 'over18=1;'}
-
-r = requests.get(my_url , headers = my_headers)
-soup = BeautifulSoup(r.text, "html5lib")
-reser = soup.select('.push')
-shhh = 0
-push = 0
-shhh_words = []
-push_words = []
-word_count = {}
-for i in reser:
-
-    try:
-        if '噓' in str(i.find('span' , class_='f1 hl push-tag').get_text()) :
-            shhh += 1
-            txt = str(i.find('span' , class_='f3 push-content').get_text().replace(':','').replace(' ',''))
-            shhh_words.append(txt)
-
-    except:
-        if '推' in str(i.find('span' , class_='hl push-tag').get_text()) :
-            push += 1
-            txt = str(i.find('span' , class_='f3 push-content').get_text().replace(':','').replace(' ',''))
-            push_words.append(txt)
-
-for i in push_words:
-    s1_list = jieba.cut(i, cut_all =True)
-    for n  in s1_list :
-        if n in word_count:
-            word_count[n] +=1
-        else:
-            word_count[n] = 1
-print(sorted(word_count.items(), key=lambda x:x[1]))
-#print(sorted(word_count.keys()))
-#print(word_count)
-'''
